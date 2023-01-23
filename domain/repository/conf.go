b33017c435f01b0f88548128a30124171696c436
SAFRIZAL AJI <safrizalaji16@gmail.com>
2023-01-23 23:52:33 +0700

    solve bugs

--- domain/repository/conf.go
+++ domain/repository/conf.go
@@ -2,10 +2,16 @@ package repository
 
 type repositoryPool struct {
 	Speciality SpecialityRepository
+	Doctor     DoctorRepository
+	Patient    PatientRepository
+	Booking    BookingRepository
 }
 
 func InitRepositoryInstance() *repositoryPool {
 	return &repositoryPool{
 		Speciality: NewSpecialityRepository(),
+		Doctor:     NewDoctorRepository(),
+		Patient:    NewPatientRepository(),
+		Booking:    NewBookingRepository(),
 	}
 }
--- main.go
+++ main.go
@@ -15,8 +15,18 @@ func main() {
 	defer dbInstance.Close()
 
 	rp := repository.InitRepositoryInstance()
+
 	specialitySrv := services.NewSpecialityService(rp.Speciality)
 	specialityCtl := controllers.NewSpecialityController(specialitySrv)
+
+	doctorSrv := services.NewDoctorService(rp.Doctor)
+	doctorCtl := controllers.NewDoctorController(doctorSrv)
+
+	patientSrv := services.NewPatientService(rp.Patient)
+	patientCtl := controllers.NewPatientController(patientSrv)
+
+	bookingSrv := services.NewBookingService(rp.Booking)
+	bookingCtl := controllers.NewBookingController(bookingSrv)
 
 	specialities := r.Group("/specialities")
 	{
@@ -25,6 +35,33 @@ func main() {
 		specialities.GET(":id", specialityCtl.ReadDetailSpeciality)
 		specialities.DELETE(":id", specialityCtl.DeleteSpeciality)
 		specialities.PUT(":id", specialityCtl.EditSpeciality)
+	}
+
+	doctors := r.Group("/doctors")
+	{
+		doctors.GET("", doctorCtl.ReadAllDoctors)
+		doctors.POST("", doctorCtl.CreateDoctor)
+		doctors.GET(":id", doctorCtl.ReadDetailDoctor)
+		doctors.DELETE(":id", doctorCtl.DeleteDoctor)
+		doctors.PUT(":id", doctorCtl.EditDoctor)
+	}
+
+	patients := r.Group("/patients")
+	{
+		patients.GET("", patientCtl.ReadAllPatients)
+		patients.POST("", patientCtl.CreatePatient)
+		patients.GET(":id", patientCtl.ReadDetailPatient)
+		patients.DELETE(":id", patientCtl.DeletePatient)
+		patients.PUT(":id", patientCtl.EditPatient)
+	}
+
+	bookings := r.Group("/bookings")
+	{
+		bookings.GET("", bookingCtl.ReadAllBookings)
+		bookings.POST("", bookingCtl.CreateBooking)
+		bookings.GET(":id", bookingCtl.ReadDetailBooking)
+		bookings.DELETE(":id", bookingCtl.DeleteBooking)
+		bookings.PUT(":id", bookingCtl.EditBooking)
 	}
 
 	r.Run()
